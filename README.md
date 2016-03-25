268d01061633cba1932f4e74ce97b8c4425c1772
Johann Burkard <johann@johannburkard.de>
2016-03-25 13:45:36 +0100

    Bigger refactoring.

--- README.md
+++ README.md
@@ -3,29 +3,43 @@
 Tiny JavaScript client for the [Google Measurement Protocol](https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters). Originally written for [EAIO](http://eaio.com/?utm_source=github&utm_medium=open-source&utm_campaign=tinymeasurement)'s widget.
 Useful if you want to ping back some data from your widget into your Google Analytics account, without relying on ga.js or analytics.js being present.
 
+*Attention: The API has changed on 2015-03-25.*
+
 ## Features
 
-* Respects DoNotTrack
-* Event tracking
 * Page view tracking
+* Event tracking
 * Exception tracking
-* User timing tracking (sampled at 20 %)
+* Social tracking
+* User timing tracking
+* HTML 5 Resource timing tracking
+* HTML 5 Page timing tracking 
+* Respects DoNotTrack
 * Survives Google Closure with Advanced Optimizations
-* HTML 5 Resource Timing API tracking based on regular expressions
-* Parameters can be overwritten through a separate object
-* Less than 1 KB post gzip
+* All parameters can be overwritten
+* Uses `navigator.sendBeacon` by default
+* About 1 KB post gzip
 
 ## Usage
 
-- Chose a namespace to export the functions under (default is "eaio")
-- Add your own property ID
-- You may want to change the host name
+- Set the Google Analytics property ID
+- Optional: Chose a namespace to export the functions under (default is "eaio")
+
 
 ```JavaScript
-})(window, 'eaio', 'UA-XXXXXXX-XX', location.host) <-- edit these!
+...
+})(window, 'eaio', 'UA-XXXXXXX-XX') <-- edit this!
 ```
 
 ## Examples
+
+### Page view tracking
+
+```JavaScript
+eaio.track.pageview('http://guh.com', 'Guh - literally the best Guh EVER!')
+```
+
+Arguments: Location, title, parameters (optional)
 
 ### Event Tracking
 
@@ -33,11 +47,7 @@ Useful if you want to ping back some data from your widget into your Google Anal
 eaio.track.event('fnuh', 'guh', location.host)
 ```
 
-### Page view tracking
-
-```JavaScript
-eaio.track.pageview('http://guh.com', 'Guh - literally the best Guh EVER!')
-```
+Arguments: Event category, event action, event label, event value (optional), nonInteraction (optional), parameters (optional)
 
 ### Exception tracking
 
@@ -46,9 +56,11 @@ try {
  pickedUpTheTelephoneThenDialedTheSevenDigits()
 }
 catch (e) {
- eaio.track.exception('yo', e, location.href)
+ eaio.track.exception('yo', e, "it's marquis baby")
 }
 ```
+
+Arguments: Event category, exception, event label (optional), parameters (optional)
 
 Some stack trace mangling is done in order to transport as much information as possible to Google Analytics.
 
@@ -65,25 +77,17 @@ catch (e) { /* Browser doesn't support the HTML 5 timing APIs which may happen *
 </script>
 ```
 
+Arguments: Timing category, timing variable, time, timing label (optional), parameters (optional)
+
+### Social tracking
+
+```JavaScript
+eaio.track.social('facebook', 'like', location.href)
+```
+
+Arguments: Social network, social action, social target (optional), parameters (optional)
+
 ### HTML 5 Resource Timing tracking
-
-#### Resource performance tracking by regular expression
-
-```HTML
-<script src="//some-shady-3rd-party-server.com/weird-widget-that-we-dont-trust.js"></script>
-<script>
-eaio.trackResourcePerformance(/3rd-party-server/, 'That damn 3rd party script')
-</script>
-```
-(This only works if the server delivers the script with a `Timing-Allow-Origin: *` header. Many CDNs do.)
-
-#### Resource performance tracking for the current page
-
-```HTML
-<script>
-eaio.trackPagePerformance('expensive Page')
-</script>
-```
 
 Timing information includes:
 
@@ -92,12 +96,43 @@ Timing information includes:
 * SSL handshake (excluding the TCP handshake time, which is different from Souders' code that I stole)
 * TTFB (time to first byte)
 
-### Passing custom parameters
+#### Resource performance tracking by regular expression
 
-```JavaScript
-eaio.trackResourcePerformance(/3rd-party-server/, 'Yet another 3rd party script', null, { 'dh': '3rd-party-server.com' })
+```HTML
+<script src="//some-shady-3rd-party-server.com/weird-widget-that-we-dont-trust.js"></script>
+<script>
+eaio.track.resourcePerformance(/3rd-party-server/, 'That damn 3rd party script')
+</script>
 ```
 
-Tracks the resource performance under the `3rd-party-server.com` hostname. Useful if you set up different profiles for various components.
+Arguments: Pattern, name, label (optional), parameters (optional)
+
+(This only works if the server delivers the script with a `Timing-Allow-Origin: *` header. Many CDNs do.)
+
+#### Resource performance tracking for the current page
+
+```HTML
+<script>
+eaio.track.pagePerformance('expensive Page')
+</script>
+```
+
+Arguments: Name, label (optional), parameters(optional)
+
+### Parameters
+
+```JavaScript
+eaio.track.resourcePerformance(/3rd-party-server/, 'Yet another 3rd party script', null, { 'dh': '3rd-party-server.com' })
+```
+
+Tracks the resource performance under the `3rd-party-server.com` host name. Useful if you set up different views.
+
+### Timing sampling rate
+
+By default, 20 % of all calls to timing methods will be sent to Google Analytics. To change this, set:
+
+```JavaScript
+eaio.track.timingSamplingRate = .1
+```
 
 [![Analytics](https://ga-beacon.appspot.com/UA-7427410-89/tinymeasurement/README.md?pixel)](https://github.com/igrigorik/ga-beacon)
--- tinymeasurement.js
+++ tinymeasurement.js
@@ -1,4 +1,4 @@
-(function(window, name, tid, dh) {
+(function(window, name, tid) {
 
     var eaio = window[name] = window[name] || {}
 
@@ -20,19 +20,19 @@
         str = []
         for (p in obj) {
             if (obj.hasOwnProperty(p) && obj[p] != undefined && obj[p] !== '') {
-                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]))
+                str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]))
             }
         }
-        return str.join("&")
+        return str.join('&')
     }
 
     function abbreviate(x, maxlength) {
         return (x || '').length > maxlength ? x.substr(0, maxlength - 3) + '...' : x
     }
 
-    eaio.track = function(params, extra) {
+    function track(params, extra) {
         if (1 == navigator['doNotTrack']) return
-        var url = 'https://www.google-analytics.com/collect?' + serialize(eaio.extend({}, eaio.track.defaultParams, params, extra))
+        var url = 'https://www.google-analytics.com/collect?' + serialize(eaio.extend({}, eaio['track']['defaultParams'], params, extra))
         try {
             navigator.sendBeacon(url)
         }
@@ -40,31 +40,9 @@
             new Image().src = url
         }
     }
-    eaio.track['defaultParams'] = { 'v': 1, 'tid': tid, 'dh': dh, 'cid': +new Date, 'aip': 1, 'ul': navigator['userLanguage'] || navigator['language'] }
-    eaio.track['timingSamplingRate'] = .2
-    eaio.track['pageview'] = function(url, title, params) {
-        eaio.track({ 't': 'pageview', 'dl': abbreviate(url, 2048), 'dt': abbreviate(title, 1500) }, params)
-    }
-    eaio.track['event'] = function(category, action, label, value, nonInteraction, params) {
-        eaio.track({ 't': 'event', 'ec': abbreviate(category, 150), 'ea': abbreviate(action, 500), 'el': abbreviate(label, 500), 'ev': value, 'ni': nonInteraction ? 1 : 0 }, params)
-    }
-    eaio.track['timing'] = function(category, variable, time, label, params) {
-        // See https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingTiming
-        if (time < 1000 * 60 * 60 && Math.random() < eaio.track['timingSamplingRate']) {
-            eaio.track({ 't': 'timing', 'utc': abbreviate(category, 150), 'utv': abbreviate(variable, 500), 'utt': time, 'utl': abbreviate(label, 500) }, params)
-        }
-    }
-    eaio.track['exception'] = function(category, thrown, label, params) {
-        try {
-            var message = thrown['message'] ? thrown['name'] + ": " + thrown['message'] : /^([^\r\n]+)/.exec(thrown['stackTrace'] || thrown['stack'])[1]
-            var stack = (thrown['stackTrace'] || thrown['stack']).replace(/^[^:]+: [^\r\n]+/, '').replace(/\s\(([^)]+)\)/g, '@$1').replace(/[\r\n](\s*at)?\s+/g, ' > ').replace(/https?:\/\/[^/]+/ig, '...')
-            eaio.track.event(category, message + " " + stack, label, null, null, params) 
-        }
-        catch (e) {}
-    }
 
     function trackPerformance(r0, name, label, params) {
-        var timing = eaio.track.timing
+        var timing = eaio['track']['timing']
         if (r0.requestStart) {
             timing(name, 'DNS', (r0.domainLookupEnd - r0.domainLookupStart)+.5|0, label, params)
             timing(name, 'TCP', ((r0.secureConnectionStart ? r0.secureConnectionStart : r0.connectEnd) - r0.connectStart)+.5|0, label, params)
@@ -75,33 +53,62 @@
         }
     }
 
-    /**
-     * Performance tracking of loaded resources.
-     * 
-     * See http://www.stevesouders.com/blog/2014/08/21/resource-timing-practical-tips/ (used with changes to the TCP time calculation).
-     */
-    eaio['trackResourcePerformance'] = function(pattern, name, label, params) {
-        try {
-            var resourceEntries = window.performance.getEntriesByType('resource'), i, r0, timing = eaio.track.timing
-            for (i = 0; i < resourceEntries.length; ++i) {
-                r0 = resourceEntries[i]
-                if (pattern.test(r0.name)) {
-                    trackPerformance(r0, name, label, params)
-                    break
+    eaio['track'] = {
+            'defaultParams': { 'v': 1, 'tid': tid, 'cid': +new Date, 'aip': 1, 'ul': navigator['userLanguage'] || navigator['language'],
+                'sr': [ screen['width'], screen['height'] ].join('x'), 'dl': abbreviate(location['href'], 2048), 'dt': abbreviate(document['title'], 1500) },
+            'timingSamplingRate': .2,
+            'pageview': function(url, title, params) {
+                track({ 't': 'pageview', 'dl': abbreviate(url, 2048), 'dt': abbreviate(title, 1500) }, params)
+            },
+            'event': function(category, action, label, value, nonInteraction, params) {
+                track({ 't': 'event', 'ec': abbreviate(category, 150), 'ea': abbreviate(action, 500), 'el': abbreviate(label, 500), 'ev': value, 'ni': nonInteraction ? 1 : 0 }, params)
+            },
+            'timing': function(category, variable, time, label, params) {
+                // See https://developers.google.com/analytics/devguides/collection/gajs/gaTrackingTiming
+                if (time < 1000 * 60 * 60 && Math.random() < eaio['track']['timingSamplingRate']) {
+                    track({ 't': 'timing', 'utc': abbreviate(category, 150), 'utv': abbreviate(variable, 500), 'utt': time, 'utl': abbreviate(label, 500) }, params)
                 }
+            },
+            'exception': function(category, thrown, label, params) {
+                try {
+                    var message = thrown['message'] ? thrown['name'] + ': ' + thrown['message'] : /^([^\r\n]+)/.exec(thrown['stackTrace'] || thrown['stack'])[1]
+                    var stack = (thrown['stackTrace'] || thrown['stack']).replace(/^[^:]+: [^\r\n]+/, '').replace(/\s\(([^)]+)\)/g, '@$1').replace(/[\r\n](\s*at)?\s+/g, ' > ').replace(/https?:\/\/[^/]+/ig, '...')
+                    eaio['track']['event'](category, message + ' ' + stack, label, null, null, params) 
+                }
+                catch (e) {}
+            },
+            'social': function(network, action, target, params) {
+                track({ 't': 'social', 'sn': abbreviate(network, 50), 'sa': abbreviate(action, 50), 'st': abbreviate(target || location['href'], 2048) }, params)
+
+            },
+            /**
+             * Performance tracking of loaded resources.
+             * 
+             * See http://www.stevesouders.com/blog/2014/08/21/resource-timing-practical-tips/ (used with changes to the TCP time calculation).
+             */
+            'resourcePerformance': function(pattern, name, label, params) {
+                try {
+                    var resourceEntries = window.performance.getEntriesByType('resource'), i, r0
+                    for (i = 0; i < resourceEntries.length; ++i) {
+                        r0 = resourceEntries[i]
+                        if (pattern.test(r0.name)) {
+                            trackPerformance(r0, name, label, params)
+                            break
+                        }
+                    }
+                }
+                catch (e) {}
+            },
+            /**
+             * Performance tracking of the page itself.
+             */
+            'pagePerformance': function(name, label, params) {
+                try {
+                    trackPerformance(window.performance.timing, name, label, params)
+                }
+                catch (e) {}
             }
-        }
-        catch (e) {}
-    }
-    
-    /**
-     * Performance tracking of the page itself.
-     */
-    eaio['trackPagePerformance'] = function(name, label, params) {
-        try {
-            trackPerformance(window.performance.timing, name, label, params)
-        }
-        catch (e) {}
     }
 
-})(window, 'eaio', 'UA-XXXXXXX-XX', location.hostname)
+
+})(window, 'eaio', 'UA-XXXXXXX-XX')
